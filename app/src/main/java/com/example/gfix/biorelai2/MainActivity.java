7932293eb858693845a6217293630706cb144ef2
Thoogui <thoogui@outlook.fr>
2019-03-22 15:24:07 +0100

    Texte button layout

--- app/src/main/java/com/example/gfix/biorelai2/MainActivity.java
+++ app/src/main/java/com/example/gfix/biorelai2/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.gfix.biorelai2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
@@ -9,6 +12,14 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button buttonMesContacts = (Button) findViewById(R.id.quitter);
+        buttonMesContacts.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
     }
 }
