4d176be4ce220adb3bfb5626273adb5386b92196
cguestereguy <clement.guestereguy@gmail.com>
2019-05-03 16:50:18 +0200

    toutes les classes

--- app/src/main/java/com/example/gfix/biorelai2/ConsulterCommandeActivity.java
+++ app/src/main/java/com/example/gfix/biorelai2/ConsulterCommandeActivity.java
@@ -1,7 +1,10 @@
 package com.example.gfix.biorelai2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
@@ -45,18 +48,29 @@ public class ConsulterCommandeActivity extends Activity {
         }
 
 
-        ArrayList<HashMap<String,String>> listeCommandes = new  ArrayList<HashMap<String,String>>();
+        final ArrayList<HashMap<String,String>> listeCommandes = new  ArrayList<HashMap<String,String>>();
         HashMap<String,String> item ;
         for(Commande uneComm : commandes){
             item = new HashMap<String,String>();
             item.put("ligne1", "Commande n°"+uneComm.getIdCommande());
             item.put("ligne2" , uneComm.getDateCommande());
+            item.put("idCommande" , uneComm.getIdCommande());
             listeCommandes.add(item);
         }
         ListView listViewCommande = (ListView) findViewById(R.id.listViewCommandes);
         SimpleAdapter adapter = new SimpleAdapter(ConsulterCommandeActivity.this, listeCommandes, android.R.layout.simple_list_item_2,
                 new String[]{"ligne1" , "ligne2"},new int[]{android.R.id.text1 , android.R.id.text2});
         listViewCommande.setAdapter(adapter);
+
+        listViewCommande.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(ConsulterCommandeActivity.this, ConsulterLigneCommandeJourActivity.class);
+                intent.putExtra("idCommande", listeCommandes.get(position).get("idCommande"));
+                intent.putExtra("log", getIntent().getStringExtra("log"));
+                startActivity(intent);
+            }
+        });
     }
 
 }
